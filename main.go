05e3e15408589a66bf7a3718fc2170c66c373add
iheartmywife <kylejchermak@gmail.com>
2026-04-30 10:26:44 -0700

    added readiness endpoint, handler func, updated Handle() pathing

--- main.go
+++ main.go
@@ -7,11 +7,16 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 	srvr := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 	serverErr := srvr.ListenAndServe()
 	if serverErr != nil {
 		log.Fatal(serverErr)
