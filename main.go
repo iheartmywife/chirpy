b9f8929803ac0271db1aa5d557bf8e92c5083a42
iheartmywife <kylejchermak@gmail.com>
2026-05-20 09:58:44 -0700

    added visit counter

--- main.go
+++ main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) ResetMetricsInc(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) GetRequestCount(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	s := strconv.Itoa(int(hits))
+	myStr := fmt.Sprintf("Hits: %s", s)
+	w.Write([]byte(myStr))
+}
+
 func main() {
+	apiConfig := apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiConfig.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	srvr := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
@@ -17,6 +43,8 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+	mux.HandleFunc("/metrics", apiConfig.GetRequestCount)
+	mux.HandleFunc("/reset", apiConfig.ResetMetricsInc)
 	serverErr := srvr.ListenAndServe()
 	if serverErr != nil {
 		log.Fatal(serverErr)
